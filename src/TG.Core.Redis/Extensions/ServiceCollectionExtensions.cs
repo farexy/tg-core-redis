c7e23d44bc10fa592366d650c2bb4525bef7a157
otopchii <farexyg@gmail.com>
2021-10-14 13:44:09 +0300

    initial setup

---
+++ src/TG.Core.Redis/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RedLockNet;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using StackExchange.Redis;
+
+namespace TG.Core.Redis.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds Redis Cache services and services that allows to create Distributed locks on Redis cache.<para></para>
+        /// Use IDatabase for accessing Redis and IDistributedLockFactory if you need distributed locking of data in Redis.
+        /// </summary>
+        public static IServiceCollection AddTgRedis(this IServiceCollection services, IConfiguration configuration) =>
+            AddTgRedis(services, configuration.GetConnectionString("Redis"));
+
+        /// <summary>
+        /// Adds Redis Cache services and services that allows to create Distributed locks on Redis cache.<para></para>
+        /// Use IDatabase for accessing Redis and IDistributedLockFactory if you need distributed locking of data in Redis.
+        /// </summary>
+        public static IServiceCollection AddTgRedis(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IDatabase>(p =>
+            {
+                var redis = ConnectionMultiplexer.Connect(connectionString);
+                return redis.GetDatabase();
+            });
+            services.AddSingleton<IDistributedLockFactory>(p =>
+            {
+                var redis = ConnectionMultiplexer.Connect(connectionString);
+                return RedLockFactory.Create(new List<RedLockMultiplexer> { redis });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/TG.Core.Redis/TgCache.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using IRedisCache = StackExchange.Redis.IDatabase;
+
+namespace Flow.Core.Redis
+{
+    public abstract class TgCache
+    {
+        private readonly IRedisCache _redisCache;
+
+        protected TgCache(IRedisCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        protected abstract string CacheKey { get; }
+        protected abstract Task ReloadCacheDataAsync();
+
+        protected async Task<TReturn> ExecuteSafe<TReturn>(Func<Task<TReturn>> action)
+        {
+            if (await _redisCache.KeyExistsAsync(CacheKey))
+            {
+                return await action.Invoke();
+            }
+
+            await ReloadCacheDataAsync();
+            return await action.Invoke();
+        }
+
+        protected async Task ExecuteSafe(Func<Task> action)
+        {
+            if (await _redisCache.KeyExistsAsync(CacheKey))
+            {
+                await action.Invoke();
+            }
+
+            await ReloadCacheDataAsync();
+            await action.Invoke();
+        }
+    }
+}
